3c67b4d9f3d8012df72a9d6d728661a71e199619
Дюша <58334576+Menxy@users.noreply.github.com>
2020-04-26 14:08:54 +0300

    Rework Slider

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 $(document).ready(function(){
     $('.get__slider').slick({
         arrows: true,
-        dots: false, //Положение меняется стилизацией контейнера .slick-dots
-        //adaptiveHeight: false, // true для slick-list
-        slidesToShow: 10, //Количество отображаемых слайдов
-        slidesToScroll: 10, //Количество слайдов перемотки
+        dots: true,//положение меняется стилизацией контейнера .slick-dots
+        //adaptiveHeight: false, // если true для slick-track align-items: flex-start;
+        slidesToShow: 6, //количество отображаемых слайдов
+        // slidesToScroll: 10, //количество слайдов перемотки
         speed: 1000, // 300 умолчание в ms
-        easing: "ease", //как в анимации css
-        infinite: true, // бесконечная перемотка (false)
-        initialSlide: 2, //стартовый слайд
-        autoplay: false,  
+        // easing: "ease", //как в анимации css
+        infinite: true, // бесконечная перемотка (true)
+        // initialSlide: 2, //стартовый слайд
+        //autoplay: true,  
         autoplaySpeed: 2000, //ms, к autoplay
         pauseOnFocus: true, // к autoplay
         pauseOnHover: true, // к autoplay
         pauseOnDotsHover: true, // к autoplay
-        draggable: true, //перетаскивание мышкой
+        draggable: true, //перетаскивание мышкой на ПК
         swipe: true, //перетаскивание тачскрином
-        touchTheshold: 5, //ширина свайпа
-        touchMove: true, //Включение передвижения тачскрином
-        waitForAnimate: true, // Быстро кликаешь - ждёт анимацию
-        //centerMode: false, //Активный слайд по центру. Показывает 3 слайда. Получает класс .slick-center
-        //variableWidth: false, // Сбрасывает width.
-        rows: 1, //Можно больше. Ряды слайдов.
-        //slidesPerRow: 1,
-        vertical: false, //Вертикальный слайдер. Нужно убрать Flex в ленте слайдов .slick-track
-        verticalSwiping: false, //Вертикальный свайп для мобил
+        // touchTheshold: 5, //ширина свайпа
+        // touchMove: true, //включение передвижения тачскрином
+        // waitForAnimate: true, // быстро кликаешь - ждёт анимацию
+        centerMode: true, //активный слайд по центру. Показывает 3 слайда. Получает класс .slick-center
+        variableWidth: true, // сбрасывает width. Хорошо работает с centerMode
+        // rows: 1, //можно больше. Ряды слайдов.
+        // slidesPerRow: 1,
+        // vertical: false, //вертикальный слайдер. Нужно убрать Flex в ленте слайдов .slick-track
+        // verticalSwiping: false, //вертикальный свайп для мобил
+        //fade: true, //только 1 слайд, остальные затемняются
+        //appendArrows:$(.get), //переместить стрелки в другой блок
+        //asNavFor:".other__slider",//связать навигацию с .other__slider
     });
+
 })
